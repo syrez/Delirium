c16427037cdfb66f04e43203245462e4f320e6b8
Valerian Thomas <thomasvalerian93@gmail.com>
2017-10-30 15:33:27 +0100

    header Change

--- app.js
+++ app.js
@@ -16,21 +16,21 @@ ourRequest.onload = function() {
       var p1 = document.createElement("p");
       var h3 = document.createElement("h3");
       var p2 = document.createElement("p");
-      var button=document.createElement("button");
+      
       newDiv.classList.add("event");
       p1.classList.add("dateTime");
       h3.classList.add("eventTitle");
       p2.classList.add("eventInfo");
-      button.classList.add("eventButton");
+
       p1.appendChild(document.createTextNode(globalData[i].date));
       p2.appendChild(document.createTextNode(globalData[i].info));
       h3.appendChild(document.createTextNode(globalData[i].title));
-      button.appendChild(document.createTextNode("more info"));
+
 
       newDiv.appendChild(p1);
       newDiv.appendChild(h3);
       newDiv.appendChild(p2);
-      newDiv.appendChild(button);
+
       getEvent.appendChild(newDiv)
     }
 
